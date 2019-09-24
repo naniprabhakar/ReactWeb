dfd91aa20b05ab618c6efda2231376f7f3481df8
naniprabhakar <55038229+naniprabhakar@users.noreply.github.com>
2019-09-24 17:54:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div class="main">
+          <div class="header">
+              <div class="heading">
+                  <h1>Training</h1>
+                  <h6>Website Template</h6>
+              </div>
+              <div class="heading1">
+                  <ul class="text1">
+                      <li class="text2"><a href="https://www.javatpoint.com/nodejs-tutorial">Node Js</a></li>
+                      <li class="text2"><a href="https://www.javatpoint.com/reactjs-tutorial">React Js</a></li>
+                      <li class="text2"><a href="https://www.javatpoint.com/angularjs-tutorial">Angular Js</a></li>
+                      <li class="text2"><a href="https://www.javatpoint.com/react-native-tutorial">React Native</a></li>
+                      <li class="text2"><a href="https://www.javatpoint.com/blockchain-tutorial">BlockChain</a></li>
+                  </ul>
+              </div>
+          </div><br /><br />
+              <div class="box">
+                  <div class="box1"><div class="boxing">215*315</div><p>IMAGE CAPTION HERE</p></div>
+                  <div class="box1"><div class="boxing">215*315</div><p>IMAGE CAPTION HERE</p></div>
+                  <div class="box1"><div class="boxing">215*315</div><p>IMAGE CAPTION HERE</p></div>
+                  <div class="box1"><div class="boxing">215*315</div><p>IMAGE CAPTION HERE</p></div>
+              </div>
+                     <br /><br />
+                  <p>Vestibulumaccumsan egestibulum eu justo convallis augue estas aenean elit intesque sed.
+                      Facilispede estibulum nulla orna nisl velit elit ac aliquat non tincidunt.
+                      Namjusto cras urna urnaretra lor urna neque sed quis orci nulla laoremut vitae doloreet condimentumst.</p>
+                  <br /><br /><hr /><br />
+                      <div class="Part">
+                          <nav>
+                              <p>CATEGORIES</p><hr />
+                                  <div class="navi">
+                                      <ul>
+                                          <li><a href="https://themeforest.net/category/all">FREE WEBSITE TEMPLATES</a></li>
+                                          <li><a href="https://www.free-css.com/free-css-templates">FREE CSS TEMPLATES</a></li>
+                                          <li><a href="https://all-free-download.com/free-website-templates/free-xhtml-template.html">FREE XHTML TEMPLATES</a></li>
+                                          <li><a href="https://github.com/redux-saga/redux-saga/commit/7d505f253c9f5a3ce2c3f00ac113ed7bd65b9dc9">REDUUX-SAGA</a></li>
+                                          <li><a href="https://appdividend.com/2018/10/03/redux-thunk-tutorial-example/#Redux_Thunk_Tutorial_With_Example_From_Scratch">REDUX THUNK</a></li>
+                                          <li><a href="https://www.tutorialspoint.com/ruby/ruby_input_output.htm">RUBY</a></li>
+                                          <li><a href="https://developers.google.com/web/tools/lighthouse/">LIGHTHOUSE</a></li>
+                                          <li><a href="https://www.javatpoint.com/typescript-tutorial">TYPESCRIPT</a></li>
+                                          <li><a href="https://www.javatpoint.com/javascript-tutorial">jAVASCRIPT</a></li>
+                                      </ul>
+                                  </div>    
+                          </nav>
+                              <div class="Boxs1">
+                                  <div class="Boxs2"> <div class="MP">180x150px</div> </div>
+                                  <div class="Meddel">
+                                      <h3>INDONECTETUS FACILIS LEO NIBH</h3>
+                                      <h5>This is a W3C standards compliant free HTML5 website template from <span>OS Templates</span>.
+                                          This template is distributed using a <span>website template licence</span>.</h5>
+                                  </div>
+
+                              </div>
+                              <div class="Boxs1">
+                                  <div class="Boxs2"> <div class="MP">180x150px</div> </div>
+                                  <div class="Meddle">
+                                      <h3>INDONECTETUS FACILIS LEO NIBH</h3>
+                                      <h5>This is a W3C standards compliant free HTML5 website template from <span>OS Templates</span>.
+                                          This template is distributed using a <span>website template licence</span>.</h5>
+                                  </div>
+
+                              </div>
+            
+
+                      </div>
+
+      </div>
+  );
+}
+
+export default App;
